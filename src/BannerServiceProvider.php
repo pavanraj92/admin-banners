a4b47179cb949a66aceec723ac543096bd273ab0
Jahnavi <jahnavi.mathur@dotsquares.com>
2025-09-02 11:51:54 +0530

    publish file changes

--- src/BannerServiceProvider.php
+++ src/BannerServiceProvider.php
@@ -62,12 +62,14 @@ class BannerServiceProvider extends ServiceProvider
             
         $slug = $admin->website_slug ?? 'admin';
 
+        $routeFile = base_path('Modules/Banners/routes/web.php');
+        if (!file_exists($routeFile)) {
+            $routeFile = __DIR__ . '/routes/web.php'; // fallback to package route
+        }
 
         Route::middleware('web')
             ->prefix("{$slug}/admin") // dynamic prefix
-            ->group(function () {
-                $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-            });
+            ->group($routeFile);
     }
 
     public function register()
